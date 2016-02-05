fe17678105dcf11c39db2e6451704388f660f061
npitsillos <npitsillos@hotmail.com>
2016-02-05 18:52:04 +0000

    chapter 8

--- mysite/rango/views.py
+++ mysite/rango/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rango.models import Category, Page
+from rango.forms import CategoryForm, PageForm
 
 def index(request):
     context_dict ={}
@@ -27,3 +28,43 @@ def category(request,category_name_slug):
         pass
 
     return render(request, 'rango/category.html',context_dict)
+
+def add_category(request):
+    if request.method == 'POST':
+
+        form = CategoryForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print form.errors
+            return render(request,'rango/add_category.html',{'form':form})
+    else:
+        form = CategoryForm()
+        return render(request,'rango/add_category.html',{'form':form})
+
+def add_page(request, category_name_slug):
+    try:
+        cat = Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+        cat = None
+
+    if request.method == 'POST':
+
+        form = PageForm(request.POST)
+        if form.is_valid():
+            if cat:
+                page = form.save(commit=True)
+                page.category = cat
+                page.views = 0
+                page.save()
+                return category(request, category_name_slug)
+        else:
+            print form.errors
+    else:
+        form = PageForm()
+
+        context_dict = {'form':form, 'category':cat}
+
+        return render(request, 'rango/add_page.html', context_dict)
