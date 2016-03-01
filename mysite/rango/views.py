b23be049080f8640b893b92a40df0579792cd082
npitsillos <npitsillos@hotmail.com>
2016-03-01 18:04:32 +0000

    chapter 11

--- mysite/rango/views.py
+++ mysite/rango/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from datetime import datetime
 from rango.models import Category, Page
 from django.contrib.auth import logout, authenticate, login
 from django.contrib.auth.decorators import login_required
@@ -6,30 +7,56 @@ from django.http import HttpResponse, HttpResponseRedirect
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 
 def index(request):
-    context_dict ={}
     category_list = Category.objects.order_by('-likes')[:5]
-    most_viewed = Page.objects.order_by('views')[:5]
-    context_dict['categories'] = category_list
-    context_dict['most_viewed'] = most_viewed
-    return render(request,'rango/index.html',context_dict)
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict = {'categories': category_list, 'pages': page_list}
+
+    visits = request.session.get('visits')
+    if not visits:
+        visits = 1
+    reset_last_visit_time = False
+
+    last_visit = request.session.get('last_visit')
+
+    if last_visit:
+        last_visit_time = datetime.strptime(last_visit[:-7], "%Y-%m-%d %H:%M:%S")
+
+        if (datetime.now() - last_visit_time).seconds > 0:
+            visits = visits + 1
+            reset_last_visit_time = True
+    else:
+        reset_last_visit_time = True
+
+    if reset_last_visit_time:
+        request.session['last_visit'] = str(datetime.now())
+        request.session['visits'] = visits
+    context_dict['visits'] = visits
+
+    response = render(request, 'rango/index.html', context_dict)
+    return response
 
 def about(request):
-    context_dict = {'message': "Hi"}
+
+    context_dict = {}
+    context_dict['message'] = "Hi"
+    if request.session.get('visits'):
+        count = request.session.get('visits')
+    else:
+        count = 0
+
+    context_dict['visits'] = count
     return render(request,'rango/about.html',context_dict)
 
 def category(request,category_name_slug):
     context_dict = {}
-
     try:
-        category = Category.objects.get(slug=category_name_slug)
-        context_dict['category_name'] = category.name
-
-        pages = Page.objects.filter(category=category)
+        cat = Category.objects.get(slug=category_name_slug)
+        context_dict['category_name'] = cat.name
+        pages = Page.objects.filter(category=cat)
         context_dict['pages'] = pages
-        context_dict['category'] = category
+        context_dict['category'] = cat
     except Category.DoesNotExist:
         pass
-
     return render(request, 'rango/category.html',context_dict)
 
 def add_category(request):
@@ -54,14 +81,14 @@ def add_page(request, category_name_slug):
         cat = None
 
     if request.method == 'POST':
-
         form = PageForm(request.POST)
         if form.is_valid():
             if cat:
-                page = form.save(commit=True)
+                page = form.save(commit=False)
                 page.category = cat
                 page.views = 0
                 page.save()
+
                 return category(request, category_name_slug)
         else:
             print form.errors
@@ -73,16 +100,13 @@ def add_page(request, category_name_slug):
         return render(request, 'rango/add_page.html', context_dict)
 
 def register(request):
-
     registered = False
 
     if request.method == 'POST':
         user_form = UserForm(data=request.POST)
         profile_form = UserProfileForm(data=request.POST)
-
         if user_form.is_valid() and profile_form.is_valid():
             user = user_form.save()
-
             user.set_password(user.password)
             user.save()
 
@@ -110,9 +134,10 @@ def user_login(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
+        print username + ' ' + password
 
         user = authenticate(username=username, password=password)
-
+        print user
         if user:
             if user.is_active:
                 login(request,user)
@@ -132,4 +157,4 @@ def user_logout(request):
 
 @login_required
 def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text!")
+    return render(request,'rango/restricted.html')
